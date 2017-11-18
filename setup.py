2e1a3fd15cec67787ce369b83c44da413ef417d4
Bill Kish <bill@cogniac.co>
2017-11-17 16:26:29 -0800

    add recursive upload option to cogupload

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='cogniac',
-      version='1.5.11',
+      version='1.5.12',
       description='Python SDK for Cogniac Public API',
       packages=['cogniac'],
       author = 'Cogniac Corporation',
