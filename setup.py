3b68e24cfd7fb8625112b3d64c24c1b933c6e561
Huayan Amy Wang <amy@cogniac.co>
2018-01-15 12:33:01 -0800

    up version for search_subjects fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='cogniac',
-      version='1.5.13',
+      version='1.5.14',
       description='Python SDK for Cogniac Public API',
       packages=['cogniac'],
       author = 'Cogniac Corporation',
