f1d039a98afe7366ebb3217507fe8064fb2b23bc
Bill Kish <bill@cogniac.co>
2019-01-02 14:49:10 -0800

    bump to 1.8.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='cogniac',
-      version='1.8.3',
+      version='1.8.4',
       description='Python SDK for Cogniac Public API',
       packages=['cogniac'],
       author = 'Cogniac Corporation',
