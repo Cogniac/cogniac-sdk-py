c4a8c1ad05494a68208227b48d3a512e462c1cd2
Huayan Amy Wang <amy@cogniac.co>
2019-04-05 09:47:22 -0700

    bump version, haven't relesaed to public

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='cogniac',
-      version='1.8.10',
+      version='1.8.11',
       description='Python SDK for Cogniac Public API',
       packages=['cogniac'],
       author = 'Cogniac Corporation',
