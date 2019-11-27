dad4d4c0a8924fee12388a8ee09b2fbfd05ce085
amycogniac <amy@cogniac.co>
2019-11-27 09:22:16 -0800

    bump version for network camera support

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='cogniac',
-      version='1.9.3',
+      version='1.9.4',
       description='Python SDK for Cogniac Public API',
       packages=['cogniac'],
       author = 'Cogniac Corporation',
