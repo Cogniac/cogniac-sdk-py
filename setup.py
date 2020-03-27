08fbbab5f427ae7b8c8b2fb52ec12f5bb65c7a55
spencer <spencer@cogniac.co>
2020-03-27 13:16:51 -0700

    bump version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='cogniac',
-      version='1.9.5',
+      version='1.9.6',
       description='Python SDK for Cogniac Public API',
       packages=['cogniac'],
       author = 'Cogniac Corporation',
